2535395788ebb17056582580354d75d1fd848ac7
Sawan-Mukhiya <sawanmukhiy@gmail.com>
2026-01-30 17:40:17 +0545

    Update .gitignore rules

--- stock_tax_calculator/trades/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import calculator_view, evaluation_view, dashboard_view
-
-urlpatterns = [
-    path("", calculator_view),
-    path("evaluation/", evaluation_view),
-    path("dashboard/", dashboard_view),
-]
